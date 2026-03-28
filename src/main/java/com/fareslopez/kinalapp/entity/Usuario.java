8af764c86943d06c3d644932270bde077c15eb9f
flopez-2025598 <flopez-2025598@kinal.edu.gt>
2026-03-27 21:46:44 -0600

    fix: Arregle Usuario.java

--- src/main/java/com/fareslopez/kinalapp/entity/Usuario.java
+++ src/main/java/com/fareslopez/kinalapp/entity/Usuario.java
@@ -6,21 +6,21 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name = "Usuarios")
 public class Usuario {
 
     @Id
     @Column(name = "codigo_usuario")
     private int codigoUsuario;
-    @Column
+    @Column(name = "username")
     private String usuario;
-    @Column
+    @Column(name = "password")
     private String contrasena;
-    @Column
+    @Column(name = "email")
     private String correo;
-    @Column
+    @Column(name = "rol")
     private String rol;
-    @Column
+    @Column(name = "estado")
     private int estado;
 
     public Usuario() {
@@ -36,51 +36,21 @@ public class Usuario {
         this.estado = estado;
     }
 
-    public int getCodigoUsuario() {
-        return codigoUsuario;
-    }
+    public int getCodigoUsuario() { return codigoUsuario; }
+    public void setCodigoUsuario(int codigoUsuario) { this.codigoUsuario = codigoUsuario; }
 
-    public void setCodigoUsuario(int codigoUsuario) {
-        this.codigoUsuario = codigoUsuario;
-    }
+    public String getUsuario() { return usuario; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
 
-    public String getUsuario() {
-        return usuario;
-    }
+    public String getContrasena() { return contrasena; }
+    public void setContrasena(String contrasena) { this.contrasena = contrasena; }
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
 
-    public String getContrasena() {
-        return contrasena;
-    }
+    public String getRol() { return rol; }
+    public void setRol(String rol) { this.rol = rol; }
 
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-
-    public int getEstado() {
-        return estado;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
+    public int getEstado() { return estado; }
+    public void setEstado(int estado) { this.estado = estado; }
 }
